848966f7650fb1ec6e9d47d16cdba250400a7d04
gusmmm <jgustavomartins@gmail.com>
2016-09-03 10:32:39 +0100

    cria ficheiro xlsx para analise manual

---
+++ code/grava_ficheiros_xlxs.R
@@ -0,0 +1,17 @@
+# grava ficheiros xlsx para analise manual
+
+# ficheiro principal
+principal <- fd
+
+principal$`Carimbo de data/hora` <- NULL
+principal$Diagnóstico <- NULL
+principal$Cirurgia <- NULL
+principal$`Patologia prévia` <- NULL
+principal$`Bloqueio de Nervos Periféricos/Plexo` <- NULL
+principal$`Tipo de Anestesia`<- NULL
+principal$`Via aérea` <- NULL
+principal$Notas <- NULL
+principal$`Técnicas e monitorização` <- NULL
+principal$`Acidentes e complicações` <- NULL
+principal$Alergias <- NULL
+
