be43d11f8f6c5583a7b86c17cd5ea73de7ae090d
gusmmm <jgustavomartins@gmail.com>
2016-09-03 10:40:28 +0100

    patologia previa por classe

--- patologia_previa.R
+++ patologia_previa.R
@@ -17,3 +17,26 @@ a[1]  <- "id"
 names(pp) <- a
 
 pp[is.na(pp)] <- 0
+
+#### por classe de patologia
+
+patologia_regex$classe[as.numeric(patologia_previa$n_hits)] -> por_classe
+
+pp_classe <- patologia_previa
+pp_classe$classe <- patologia_regex$classe[as.numeric(patologia_previa$n_hits)]
+pp_classe$conceito <- NULL
+pp_classe$n_hits <- NULL
+pp_classe$sim <- 1
+pp_classe$hits <- NULL
+
+pp_classe <- reshape(pp_classe,direction = "wide",idvar = "id", timevar = "classe")
+pp_classe$sim. <- NULL
+pp_classe$sim.NA <- NULL
+
+names(pp_classe)
+a <- str_extract(names(pp_classe),"\\..*") 
+a <- str_replace(a,"\\.","" )
+a[1]  <- "id"
+names(pp_classe) <- a
+
+pp_classe[is.na(pp_classe)] <- 0
